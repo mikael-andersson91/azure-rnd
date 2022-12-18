ca2459e854c3c3bafe68505e27abb891b26b22ff
miand9 <mikael.andersson3@ingka.ikea.com>
2022-12-18 20:55:09 +0100

    Update automanage_assignment.py

--- automanage_assignment.py
+++ automanage_assignment.py
@@ -7,7 +7,7 @@ from azure.mgmt.automanage import AutomanageClient
 def main():
     rg = "new-vm-rg"
     profile_name = "automanage-rnd"
-    virtual_machines = "new-vm"
+    vm = "new-vm"
     SUBSCRIPTION_ID = os.environ['AZURE_SUBSCRIPTION_ID']
 
     # Create Automanage Client
@@ -24,15 +24,15 @@ def main():
     }
 
     # Assign each virtual machine to automanage profile
-    for vm in virtual_machines:
+    #for vm in virtual_machines:
         # Assign vm to configuration profile assignment
-        client.configuration_profile_assignments.create_or_update(
-            "default", rg, vm, custom_profile_assignment)
+    client.configuration_profile_assignments.create_or_update(
+        "default", rg, vm, custom_profile_assignment)
 
         # Get configuration profile assignment
-        assignment = client.configuration_profile_assignments.get(
-            rg, "default", vm)
-        print(assignment)
+    assignment = client.configuration_profile_assignments.get(
+        rg, "default", vm)
+    print(assignment)
 
 
 if __name__ == '__main__':
