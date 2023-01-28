82f6ab71d316c9014f5ea6325b5086a51569a89f
miand9 <mikael.andersson3@ingka.ikea.com>
2023-01-28 16:09:49 +0100

    Update list_groups.py

--- list_groups.py
+++ list_groups.py
@@ -1,6 +1,7 @@
 # Import the needed credential and management objects from the libraries.
 from azure.identity import AzureCliCredential
 from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.compute import ComputeManagementClient
 import os
 
 # Acquire a credential object using CLI-based authentication.
@@ -11,6 +12,8 @@ subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
 
 # Obtain the management object for resources.
 resource_client = ResourceManagementClient(credential, subscription_id)
+
+compute_client = ComputeManagementClient(credential, subscription_id)
 
 # Retrieve the list of resource groups
 group_list = resource_client.resource_groups.list()
@@ -23,3 +26,7 @@ print("-" * (column_width * 2))
 
 for group in list(group_list):
     print(f"{group.name:<{column_width}}{group.location}")
+    virtual_machines = compute_client.virtual_machines.list(group)
+    for vm in virtual_machines:
+        print(f"Turning off + {vm.name:<{(column_width*4)}}")
+        compute_client.virtual_machines.begin_power_off(group,vm)
